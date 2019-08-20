07d9e4a6c60d925781e85e9a9488b473bf65fe1b
Elliott Stam <elliott.stam@gmail.com>
2019-08-19 17:03:44 -0700

    Modified files: LICENSE, READE.md, setup.py

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="example-pkg-divinorum-webb",
+    name="example-pkg-divinorum",
     version="0.0.1",
     author="Elliott Stam",
     author_email="elliott.stam@gmail.com",
     description="An example package for PyPI",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/divinorum-webb/example_pkg",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
